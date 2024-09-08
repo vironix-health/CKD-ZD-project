ccfc3cc2cd172bec34473e0bf5f6f28576219597
Zach Dana <zdana@ucsc.edu>
2024-09-08 10:42:03 -0700

    update driver

--- driver.py
+++ driver.py
@@ -1,7 +1,7 @@
 from components.DataHandler import DataHandler
 from components.SHAPSelector import SHAPSelector
 from components.ClassicalLearningWrapper import ClassicalLearningWrapper
-# from components.DeepLearningWrapper import DeepLearningWrapper
+from components.DeepLearningWrapper import DeepLearningWrapper
 from components.CoxPHWrapper import CoxPHWrapper
 
 
@@ -33,9 +33,9 @@ class Driver:
             Data.X_traindev,
             Data.X_test, 
         )
-        Selector.SHAP_FeatureSelect()
+        Selector.SHAP_FeatureFilter()
         Selector.plot_SHAPma()
-        Selector.plot_SHAPbeeswarm()
+        Selector.plot_ClassicalSHAPbeeswarm()
         
         # Augment base dataset with novel predictor features
         Data.baseAugmentation(Selector.get_NovelFeatures())
@@ -49,40 +49,42 @@ class Driver:
         CoxPH.plot_DynamicAUC()        
         CoxPH.plot_SurvivalCurves(2, 5) # Plot survival curves for CKD stages 2-5
 
-    # def DeepLearningPipe(self):
-    #     # Split data into training, testing, and validation sets
-    #     Data = DataHandler(self.cfg, self.master, self.base)
-    #     Data.split_ValSets()
-        
-    #     # Initialize XGBoostWrapper and perform Bayesian hyperparameter optimization
-    #     CLWrapper = ClassicalLearningWrapper(self.cfg)
-    #     CLWrapper.BayesianHyperparameterOptimizer(Data.val_sets)
-    #     CLWrapper.Trainer(Data.X_traindev, Data.y_traindev)
-    #     CLWrapper.Evaluator(Data.X_test, Data.y_test)
+    def DeepLearningPipe(self):
+        # Split data into train and test; initialize tensors and dataloaders
+        Data = DataHandler(self.cfg, self.master, self.base)
+        Data.split_Vanilla()
+        Data.tensorWorkFlow()
 
-    #     # Compute SHAP values, select novel predictor features
-    #     Selector = SHAPSelector(
-    #         self.cfg,
-    #         Data.master_features,
-    #         self.base, 
-    #         CLWrapper.model, 
-    #         Data.X_traindev,
-    #         Data.X_test, 
-    #     )
-    #     Selector.SHAP_FeatureSelect()
-    #     Selector.plot_SHAPma()
-    #     Selector.plot_SHAPbeeswarm()
+        # Initialize XGBoostWrapper and perform Bayesian hyperparameter optimization
+        DLWrapper = DeepLearningWrapper(self.cfg, Data.X_traindev.shape[1])
+        DLWrapper.Trainer(Data.train_loader, Data.test_loader)
+        DLWrapper.Evaluator(Data.test_loader)
+
+        # Compute SHAP values, select novel predictor features
+        Selector = SHAPSelector(
+            self.cfg,
+            Data.master_features,
+            self.base, 
+            DLWrapper.model, 
+            Data.X_traindev,
+            Data.X_test, 
+            Data.X_train_tensor,
+            Data.X_test_tensor
+        )
+        Selector.SHAP_FeatureFilter()
+        Selector.plot_SHAPma()
+        Selector.plot_DeepSHAPbeeswarm()
         
-    #     # Augment base dataset with novel predictor features
-    #     Data.baseAugmentation(Selector.get_NovelFeatures())
+        # Augment base dataset with novel predictor features
+        Data.baseAugmentation(Selector.get_NovelFeatures())
         
-    #     # Initialize CoxPHWrapper and evaluate on augmented dataset
-    #     CoxPH = CoxPHWrapper(self.cfg, Data.augment)
-    #     CoxPH.Summary()
-    #     CoxPH.FeatureRank()
-    #     CoxPH.SchoenfeldTest()
-    #     CoxPH.plot_BrierScore()
-    #     CoxPH.plot_DynamicAUC()        
-    #     CoxPH.plot_SurvivalCurves(2, 5) # Plot survival curves for CKD stages 2-5
+        # Initialize CoxPHWrapper and evaluate on augmented dataset
+        CoxPH = CoxPHWrapper(self.cfg, Data.augment)
+        CoxPH.Summary()
+        CoxPH.FeatureRank()
+        CoxPH.SchoenfeldTest()
+        CoxPH.plot_BrierScore()
+        CoxPH.plot_DynamicAUC()        
+        CoxPH.plot_SurvivalCurves(2, 5) # Plot survival curves for CKD stages 2-5
 
 
